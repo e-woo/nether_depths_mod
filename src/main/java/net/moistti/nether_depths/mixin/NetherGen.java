cbf4cc60fdd47d9125c3bda34be9ab44816ae0a5
moistti <moistteel@gmail.com>
2023-09-25 22:17:22 -0600

    9/23/23

--- src/main/java/net/moistti/nether_depths/mixin/NetherGen.java
+++ src/main/java/net/moistti/nether_depths/mixin/NetherGen.java
@@ -1,6 +1,9 @@
 package net.moistti.nether_depths.mixin;
 
 import net.minecraft.block.Blocks;
+import net.minecraft.fluid.Fluids;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.world.gen.chunk.AquiferSampler;
 import net.minecraft.world.gen.chunk.ChunkGeneratorSettings;
 import net.minecraft.world.gen.chunk.NoiseChunkGenerator;
@@ -19,10 +22,10 @@ public class NetherGen {
     private static void inject(ChunkGeneratorSettings settings, CallbackInfoReturnable<AquiferSampler.FluidLevelSampler> cir) {
         NetherDepths.LOGGER.info("injecting mixin!");
         int i = settings.seaLevel();
-        if (settings.defaultBlock().equals(Blocks.NETHERRACK.getDefaultState())) {
+        if (settings.defaultFluid().equals(Fluids.LAVA.getDefaultState().getBlockState())) {
             NetherDepths.LOGGER.info("in nether!");
             cir.setReturnValue((x, y, z) -> {
-                if (y < Math.min(-54, i) || (y > -5 && y < 32)) {
+                if (y < Math.min(-54, i) || (y > 0 && y < 32)) {
                     return new AquiferSampler.FluidLevel(32, Blocks.LAVA.getDefaultState());
                 }
                 return new AquiferSampler.FluidLevel(i, settings.defaultFluid());
