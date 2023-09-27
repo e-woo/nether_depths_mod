bed3bf1ad3f8518fa2a74e058b5556777cadbd5c
moistti <moistteel@gmail.com>
2023-09-27 01:27:11 -0600

    9/23/23

--- src/main/java/net/moistti/nether_depths/registers/RegisterEntities.java
+++ src/main/java/net/moistti/nether_depths/registers/RegisterEntities.java
@@ -1,13 +1,9 @@
 package net.moistti.nether_depths.registers;
 
-import net.fabricmc.fabric.api.object.builder.v1.entity.FabricDefaultAttributeRegistry;
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricEntityTypeBuilder;
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityDimensions;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.SpawnGroup;
-import net.minecraft.entity.attribute.DefaultAttributeContainer;
-import net.minecraft.entity.vehicle.BoatEntity;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
@@ -15,7 +11,7 @@ import net.moistti.nether_depths.NetherDepths;
 import net.moistti.nether_depths.entities.IronBoat;
 public final class RegisterEntities {
     public static final EntityType<IronBoat> IRON_BOAT =
-            Registry.register(Registries.ENTITY_TYPE, new Identifier(NetherDepths.MOD_ID, "boat"), FabricEntityTypeBuilder.create(
+            Registry.register(Registries.ENTITY_TYPE, new Identifier(NetherDepths.MOD_ID, "iron_boat"), FabricEntityTypeBuilder.create(
                     SpawnGroup.MISC, IronBoat::new).dimensions(EntityDimensions.fixed(1.375f, 0.5625f)).fireImmune()
             .build()
 		);
